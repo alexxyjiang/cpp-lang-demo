cbc54570ca1a38f7cb16ccbebc4aa7d4ac500a9b
Xueyuan Jiang <alexxyjiang@gmail.com>
2021-01-19 21:18:40 +0800

    support ninja make system

--- build.sh
+++ build.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 # this script requires c++ compiler and cmake system
 if [ $# -lt 1 ]; then
-  echo "usage: ${0} target-project"
+  echo "usage: ${0} target-project [make-type]"
   exit 0
 fi
 target="${1}"
+mktype="${2}"
 if [ -d "${target}" ]; then
   echo "building ${target} ..."
-  cd "${target}" && cmake -B build . && cmake --build build && cd -
+  if echo "${mktype}" | grep -iq "ninja"; then
+    cd "${target}" && cmake -G Ninja -B build . && cmake --build build && cd -
+  else
+    cd "${target}" && cmake -B build . && cmake --build build && cd -
+  fi
   echo "built status of ${target}: $?"
 else
   echo "${target} doesn't exist!"
