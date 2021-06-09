0161d33ef5864953237a62993e110c9076995dad
Xueyuan Jiang <alexxyjiang@gmail.com>
2021-06-09 16:58:24 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,22 +1,37 @@
 #!/bin/sh
 # this script requires c++ compiler and cmake system
 if [ $# -lt 1 ]; then
-  echo "usage: ${0} target-project [make-type]"
+  echo "usage: ${0} target-project [makefile-type/build-type]"
   exit 0
 fi
 target="${1}"
-mktype="${2}"
+tags="$*"
+mkfiletype="Unix Makefiles"
+buildtype="Release"
+for tag in "${tags}"; do
+  if echo "${tag}" | grep -iq "ninja"; then
+    mkfiletype="Ninja"
+  fi
+  if echo "${tag}" | grep -iq "debug"; then
+    buildtype="Debug"
+  fi
+done
+
 if [ -d "${target}" ]; then
-  echo "building ${target} ..."
-  if [ -d "${target}/build" ]; then
-    cd "${target}/build" && rm -rf * && cd -
+  echo "[${0}] build ${buildtype} ${target} binaries with ${mkfiletype} ..."
+  target_src_path="`pwd`/${target}"
+  target_bin_path="./build/${target}"
+  echo "[${0}] clean up ..."
+  if [ -d "${target_bin_path}" ]; then
+    rm -rf ${target_bin_path}
   fi
-  if echo "${mktype}" | grep -iq "ninja"; then
-    cd "${target}" && cmake -G "Ninja" -B build . && cmake --build build && cd -
-  else
-    cd "${target}" && cmake -G "Unix Makefiles" -B build . && cmake --build build && cd -
-  fi
-  echo "built status of ${target}: $?"
+  mkdir -p ${target_bin_path}
+  cd ${target_bin_path} &&
+  cmake -G "${mkfiletype}" \
+    -D CMAKE_BUILD_TYPE="${buildtype}" \
+    ${target_src_path} &&
+  cmake --build . && cd -
+  echo "[${0}] build ${buildtype} ${target} binaries with ${mkfiletype} exit code $?"
 else
-  echo "${target} doesn't exist!"
+  echo "[${0}] ${target} doesn't exist!"
 fi
