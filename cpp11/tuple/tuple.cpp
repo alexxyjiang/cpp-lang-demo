00bea2a61def7a47d46046a6b8e45b8f5567fb3e
Xueyuan Jiang <alexxyjiang@gmail.com>
2021-06-09 17:22:34 +0800

    std::tuple update

--- cpp11/tuple/tuple.cpp
+++ cpp11/tuple/tuple.cpp
@@ -5,15 +5,10 @@
 #include <iostream>
 #include <tuple>
 
-using std::cout;
-using std::endl;
-using std::tie;
-using std::tuple;
-
 int main(int argc, char **argv) {
   int res1 = 0;
-  int res2 = 0;
-  auto example = std::tuple<int, int>(2, 3);
+  double res2 = 0;
+  auto example = std::tuple<int, double>(2, 3.5);
   std::tie(res1, std::ignore) = example;
   res2 = std::get<1>(example);
   std::cout << "res1 = " << res1 << std::endl << "res2 = " << res2 << std::endl;
